40c2592a679f41b945832ea018351887b5eb1671
tinayating <yttian@cmu.edu>
2017-07-06 11:22:33 +0800

    Update palindromeNumber.java

--- palindromeNumber.java
+++ palindromeNumber.java
@@ -36,6 +36,35 @@ Improved code written by other coder.
 **/
 
 
+public class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0) return false;
+        int rev = 0;
+        while(x>rev){
+            rev= rev*10 + y%10;
+            x /= 10;
+        }
+        return rev == x || rev/10 == x;
+    }
+}
+
+/**
+Improved code version(2)  Runtime: 195ms  Beats 90.09%
+**/
+
+public class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0) return false;
+        int y = x;
+        int rev = 0;
+        while(y>0){
+            rev = rev*10 + y%10;
+            y /= 10;
+        }
+        return rev == x;
+    }
+}
+          
 
 
 
