a091a6b1e2f1cc4d43981c84a4e5aefed46e1593
tinayating <yttian@cmu.edu>
2017-07-14 18:30:54 +0800

    Update lengthOfLastWord.java

--- lengthOfLastWord.java
+++ lengthOfLastWord.java
@@ -1,7 +1,8 @@
+//runtime: 5ms, beats 67%
 public class Solution {
     public int lengthOfLastWord(String s) {
         int count = 0;
-        s = s.trim();
+        s = s.trim(); //先去掉首尾的空格，否则会出错。
         if(s == "") return 0;
         else if(s.indexOf(' ') == -1) return s.length();
         else{
