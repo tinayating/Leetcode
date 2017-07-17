3990014f800b2930455876f4ce70945e7037e4bc
tinayating <yttian@cmu.edu>
2017-07-17 16:11:35 +0800

    Create plusOne.java

---
+++ plusOne.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for(int i=n-1;i>=0;i--){
+            if(digits[i] < 9){
+                digits[i] += 1;
+                return digits;
+            }
+            digits[i] = 0;
+            
+        }
+        int newArray[] = new int[n+1];
+        newArray[0] = 1;
+        
+        return newArray;
+    }
+}
