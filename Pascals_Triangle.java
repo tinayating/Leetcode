32de29513d23a29a70e3ce7f7bca80fdd9af1153
Tina <yttian@cmu.edu>
2018-02-07 00:41:06 -0500

    Update Pascals_Triangle.java

--- Pascals_Triangle.java
+++ Pascals_Triangle.java
@@ -5,16 +5,16 @@ class Solution {
             return result;
         List<Integer> first = new ArrayList<Integer>();
         first.add(1);
-        result.add(first);
-        for(int i = 2; i <= numRows; i++){ //i is the number of row
+        result.add(first); //add the first row to the result
+        for(int i = 1; i < numRows; i++){ 
             List<Integer> row = new ArrayList<>();
             row.add(1);
-            if(i-2 > 0){
-                for(int j = 1; j < i-1; j++){
-                    row.add(result.get(i-1-1).get(j-1) + result.get(i-1-1).get(j));
+            if(i-1 > 0){
+                for(int j = 1; j < i; j++){
+                    row.add(result.get(i-1).get(j-1) + result.get(i-1).get(j));
                 }
             }
-            if(i >= 2)
+            if(i >= 1)
                 row.add(1);
             result.add(row);
         }
