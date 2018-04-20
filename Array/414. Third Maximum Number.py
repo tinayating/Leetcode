adef72479f5b32450d012ff11d11f375aa11edc5
Tina <yttian@cmu.edu>
2018-04-20 13:07:34 -0400

    Create 414. Third Maximum Number.py

---
+++ Array/414. Third Maximum Number.py
@@ -0,0 +1,21 @@
+import math
+class Solution:
+    def thirdMax(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums = set(nums)
+        v = [-math.inf, -math.inf, -math.inf]
+        for num in nums:
+            if num > v[0]:
+                v = [num, v[0], v[1]]
+            elif num > v[1]:
+                v = [v[0], num, v[1]]
+            elif num > v[2]:
+                v = [v[0],v[1],num]
+
+        if -math.inf in v:
+            return max(v)
+        else:
+            return v[2]
