b4311b476ee1dd888ad7ea19fabf006dcf9bb075
Tina <yttian@cmu.edu>
2018-04-25 18:50:45 -0400

    Update 14. Longest Common Prefix.py

--- Python/14. Longest Common Prefix.py
+++ Python/14. Longest Common Prefix.py
@@ -4,12 +4,12 @@ class Solution:
         :type strs: List[str]
         :rtype: str
         """
-        if len(strs) == 0: return ""
+        if len(strs) == 0: return ""  # input:[]
         strs = sorted(strs)
         s = strs[0]
         out = ""
         for i,c in enumerate(s):
-            for other in strs[1:]:
+            for other in strs[1:]: #character wise compare
                 if other[i] != c:
                     return out
             out += c
