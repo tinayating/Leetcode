aae20f6fadc93a2b5e1231f4edd04d43c9149cb7
Tina <yttian@cmu.edu>
2018-04-25 11:32:41 -0400

    Update 136. Single Number.py

--- Python/136. Single Number.py
+++ Python/136. Single Number.py
@@ -14,3 +14,22 @@ class Solution:
 # & AND 都为1才为1
 # | OR  只要有1就是1
 # ^ XOR  全一样为0，不一样为1，所以自己和自己XOR为0
+
+
+# use set(), require extra space
+class Solution:
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        s = set()
+        
+        for n in nums:
+            if n not in s:
+                s.add(n)
+            else:
+                s.remove(n)
+        
+        single = list(s)[0]
+        return single
